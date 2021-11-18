1f40166aea13c5157c4b216a7d447ca67d9aaf20
joinThe <45047425+joinThe@users.noreply.github.com>
2021-11-18 14:06:29 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+const detail = {
+  length: 0,
+  width: 0,
+  lt: {},
+  lb: {},
+  rt: { radius: 50 },
+  rb: {}
+};
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+const drawDetail = (detail) => {
+  //your code here....
+};
