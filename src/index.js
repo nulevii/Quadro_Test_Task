5d36c4c5e742018cbdcd337021133aee4fe4c4f3
stadnik gleb <stadnikgleb@gmail.com>
2021-11-18 14:04:51 +0200

    test detail

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import "./styles.css";
 
 const detail = {
-  length: 0,
-  width: 0,
+  length: 350,
+  width: 250,
   lt: {},
   lb: {},
   rt: { radius: 50 },
   rb: {}
 };
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
-
-const drawDetail = (detail) => {
+{
   //your code here....
-};
+  const app = document.getElementById("app");
+  const leftAngleInput = document.getElementById("leftAngleInput");
+  const rotateZ = document.getElementById("rotateZ");
+
+
+  
+}
+
